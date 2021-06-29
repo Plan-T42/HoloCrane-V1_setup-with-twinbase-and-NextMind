8744a7a647bb01c3a4bb35703287ab0b9bc327b0
Plan-T42 <shoegaze.kida@gmail.com>
2021-06-29 09:59:44 +0300

    init commit with json parser, web request and ćanvas to access sample parameters from crane's DT documents

---
+++ Assets/JsonReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class JsonReader : MonoBehaviour
+{
+    public TextAsset jsonFile;
+
+
+    [Serializable]
+    public class Twin
+    {
+        public string url;
+        public string name;
+    }
+
+    [Serializable]
+    public class Owner
+    {
+        public string dt_id;
+    }
+
+    [Serializable]
+    public class DT_document
+    {
+        public string baseurl;
+        public Owner owner;
+        public string repo_url;
+        public string new_twin_url;
+        public Twin[] twins;
+    }
+
+
+    void Start()
+    {
+        DT_document DTInJson = JsonUtility.FromJson<DT_document>(jsonFile.text);
+
+        foreach (Twin twin in DTInJson.twins)
+        {
+            Debug.Log("Found digital twin: " + twin.name + " with url: " + twin.url);
+        }
+
+        Debug.Log("Owner: " + DTInJson.owner.dt_id);
+
+    }
+}
---
+++ Assets/JsonReaderFromServer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+using SimpleJSON;
+using UnityEngine.Networking;
+using TMPro;
+
+public class JsonReaderFromServer : MonoBehaviour
+{
+    
+    public TMP_Text nameTXT;
+    public TMP_Text descriptionTXT;
+    public TMP_Text ownerTXT;
+    public TMP_Text locationTXT;
+
+    public void getJsonData()
+    {
+        Debug.Log("get JSON Data!");
+        StartCoroutine(RequestWebService());
+    }
+
+    IEnumerator RequestWebService()
+    {
+        string getDataUrl = "https://dtw.dtp.fi/crane-ilmatar/index.json";
+        //  + inputTxt.text
+        //print(getDataUrl);
+        Debug.Log("Data url: " + getDataUrl);
+
+        using (UnityWebRequest webData = UnityWebRequest.Get(getDataUrl))
+        {
+
+            yield return webData.SendWebRequest();
+            if (webData.isNetworkError || webData.isHttpError)
+            {
+                //print("---------------- ERROR ----------------");
+                //print(webData.error);
+                Debug.Log("ERROR: " + webData.error);
+            }
+            else
+            {
+                if (webData.isDone)
+                {
+                    JSONNode jsonData = JSON.Parse(System.Text.Encoding.UTF8.GetString(webData.downloadHandler.data));
+
+                    if (jsonData == null)
+                    {
+                        Debug.Log("NO DATA");
+                    }
+                    else
+                    {
+                        Debug.Log("JSON DATA: ");
+                        Debug.Log("jsonData.Count:" + jsonData.Count);
+
+                        nameTXT.text = jsonData["name"];
+                        Debug.Log("digital twin name: " + name);
+
+                        descriptionTXT.text = jsonData["description"]; 
+                        Debug.Log("desciption: " + jsonData["description"]);
+
+                        ownerTXT.text = jsonData["owner"]["name"];
+                        Debug.Log("desciption: " + jsonData["owner"]["name"]);
+
+                        locationTXT.text = jsonData["location"]["streetAddress"];
+                        Debug.Log("desciption: " + jsonData["location"]["streetAddress"]);
+                    }
+                }
+            }
+        }
+    }
+}
